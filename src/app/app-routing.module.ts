9851c91fe6d636cc79ca3bc5251ac317d67cb89d
Reid Durden <reid3849@hotmail.com>
2018-08-26 16:02:21 +1000

    Project Remade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'chat', component:ChatComponent},
+  {path:'**', redirectTo:'login'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
